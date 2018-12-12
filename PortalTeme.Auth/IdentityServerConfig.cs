35628292198fb424ee9edaa9f05502298a6fb23a
Robert Dima <robert.dima@dnnsharp.com>
2018-12-13 00:20:51 +0200

    Separate IdentityServer from the Angular/MVC application project.
    + That was done by adding the Auth project.

---
+++ PortalTeme.Auth/IdentityServerConfig.cs
@@ -0,0 +1,71 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using PortalTeme.Common.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalTeme.Auth {
+    public class IdentityServerConfig {
+
+        public static IEnumerable<IdentityResource> GetIdentityResources() {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources() {
+            return new List<ApiResource> {
+                new ApiResource{
+                    Name = AuthorizationConstants.ApplicationMainApi_Name,
+                    DisplayName = "Portal Teme Main API",
+                    Scopes = {
+                        new Scope {
+                            Name = AuthorizationConstants.ApplicationMainApi_FullAccessScope,
+                            DisplayName = "Full Access to Portal Teme Main API"
+                        },
+                        new Scope {
+                            Name = AuthorizationConstants.ApplicationMainApi_ReadOnlyScope,
+                            DisplayName = "Read Only access to Portal Teme Main API"
+                        }
+                    }
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients() {
+            return new List<Client> {
+                new Client {
+                    ClientId = AuthorizationConstants.AngularAppClientId,
+                    ClientName = "Portal Teme WebApp",
+                    ClientUri = AuthorizationConstants.AngularAppRootUrl,
+
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    AllowOfflineAccess = true,
+
+                    //TODO: enable when the UI is done
+                    RequireConsent = false,
+
+                    ClientSecrets = {
+                        new Secret("secret".Sha256())
+                    },
+
+                    RedirectUris = { AuthorizationConstants.AngularAppLoginCallback },
+                    PostLogoutRedirectUris = { AuthorizationConstants.AngularAppLogoutCallback },
+                    
+                    AllowedScopes = {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        AuthorizationConstants.ApplicationMainApi_FullAccessScope,
+                        AuthorizationConstants.ApplicationMainApi_ReadOnlyScope
+                    }
+                }
+            };
+        }
+
+    }
+}
---
+++ PortalTeme.Auth/Startup.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PortalTeme.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalTeme.Auth {
+    public class Startup {
+
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services) {
+
+            services.AddDbContext<IdentityContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("IdentityContextConnection"),
+                    sqlOpts => sqlOpts.MigrationsAssembly("PortalTeme.Auth")
+                )
+            );
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityContext>()
+                .AddDefaultTokenProviders()
+                .AddDefaultUI();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddIdentityServer(options =>
+                {
+                    options.UserInteraction.LoginUrl = "/Identity/Account/Login";
+                    options.UserInteraction.LogoutUrl = "/Identity/Account/Logout";
+                })
+                .AddDeveloperSigningCredential() // TODO: Replace with .AddSigningCredential()
+                .AddInMemoryPersistedGrants()
+                .AddInMemoryIdentityResources(IdentityServerConfig.GetIdentityResources())
+                .AddInMemoryApiResources(IdentityServerConfig.GetApiResources())
+                .AddInMemoryClients(IdentityServerConfig.GetClients())
+                .AddAspNetIdentity<User>();
+
+            services.AddAuthentication(IdentityConstants.ApplicationScheme);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+
+                app.UseDatabaseErrorPage();
+            } else {
+                app.UseExceptionHandler("/Error");
+
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseIdentityServer();
+
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}"
+                );
+
+                routes.MapRoute(
+                    name: "areasDefault",
+                    template: "{area:exists}/{controller}/{action=Index}/{id?}"
+                );
+            });
+        }
+    }
+}
