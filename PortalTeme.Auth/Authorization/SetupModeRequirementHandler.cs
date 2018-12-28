1cae8a40717c25c9a4bdf3e2441f6d92f3ee5408
Robert Dima <robert.dima@dnnsharp.com>
2018-12-28 04:44:55 +0200

    Add Setup state to the Auth app.
    + Add the Admin button in the app navbar

---
+++ PortalTeme.Auth/Authorization/SetupModeRequirementHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using PortalTeme.Auth.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace PortalTeme.Auth.Authorization {
+    public class SetupModeRequirementHandler : AuthorizationHandler<SetupModeRequirement> {
+        private readonly AppInitialization init;
+
+        public SetupModeRequirementHandler(AppInitialization init) {
+            this.init = init;
+        }
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, SetupModeRequirement requirement) {
+            if (!await init.IsInitialized())
+                context.Succeed(requirement);
+        }
+    }
+}
