2962c51d19bbd2b621aea10e3d248a4a9518ce6b
Robert Dima <robert.dima@dnnsharp.com>
2019-01-05 15:12:50 +0200

    Fix the models for the course-owners component.

--- PortalTeme/ClientApp/src/app/models/course.model.ts
+++ PortalTeme/ClientApp/src/app/models/course.model.ts
@@ -1,7 +1,32 @@
 import { NamedModel } from './base.model';
 
 export interface Course extends NamedModel {
+    professor: string;
+    assistants: Assistant[];
 
+    groups: StudyGroupRef[];
+    students: StudentRef[];
+}
 
+export interface Professor {
+    professorId: string;
+    firstName: string;
+    lastName: string;
+}
 
+export interface Assistant {
+    assistantId: string;
+    firstName: string;
+    lastName: string;
+}
+
+export interface StudentRef {
+    studentId: string;
+    firstName: string;
+    lastName: string;
+}
+
+export interface StudyGroupRef {
+    groupId: string;
+    name: string;
 }
